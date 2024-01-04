388dcd8ab4c2c7f1ddd03a98319f5e34739a562b
Vedant Chainani <vedantchainani1084@gmail.com>
2024-01-05 04:27:19 +0530

    add register link

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import { Button } from '../ui/button';
+import Link from 'next/link';
 
 const Navbar = () => {
 	return (
 		<div className='absolute top-0 z-10 w-full'>
 			<div className='flex flex-row justify-end px-8 py-4'>
-				<Button className='font-medium'>Register</Button>
+				<Button className='font-medium' asChild>
+					<Link href='/register'>Register</Link>
+				</Button>
 			</div>
 		</div>
 	);
